86b38235f0ab61646d6b6322d80db65c0c1c5139
Johan Ekman <ekman-johan020@hotmail.com>
2022-08-24 22:12:18 +0200

    Implemented functions for config

--- src/config.rs
+++ src/config.rs
@@ -1,51 +1,60 @@
-use clap::ArgMatches;
 use crate::systems::printer::print;
+use clap::ArgMatches;
 
 #[derive(Debug)]
 pub struct Config {
-  length: Option<usize>,
-  algorithm_type: Option<String>,
-  hash: Option<String>,
-  list_all: bool,
+    length: Option<usize>,
+    algorithm_type: Option<String>,
+    hash: Option<String>,
+    list_all: bool,
 }
 
 impl Config {
+    pub fn scan(&self) {
+        self.check_for_print_flags();
+    }
 
-  pub fn scan(&self) {
-    self.check_for_print_flags();
-    self.exit_if_hash_is_unavailable();
-  }
-  
-  fn check_for_print_flags(&self) {
-      if self.list_all {
-          print::available_algorithms();
-          std::process::exit(1);
-      }
-  }
+    fn check_for_print_flags(&self) {
+        if self.list_all {
+            print::available_algorithms();
+            std::process::exit(1);
+        }
+    }
 
-  fn exit_if_hash_is_unavailable(&self) {
-      if self.hash.is_none() {
-          println!("No hash was supplied, (-h, --help for options)");
-          std::process::exit(1);
-      }
-  }
+    pub fn get_algorithm_type(&self) -> &Option<String> {
+        &self.algorithm_type
+    }
+
+    pub fn get_hash(&self) -> &str {
+        match &self.hash {
+            Some(hash) => return &hash,
+            None => {
+                println!("No hash was supplied, (-h, --help for options)");
+                std::process::exit(1);
+            }
+        }
+    }
+
+    pub fn get_length(&self) -> Option<usize> {
+        self.length
+    }
 }
 
 impl From<ArgMatches> for Config {
-  fn from(m: ArgMatches) -> Self {
-      exit_if_empty(&m);
-      Config {
-          length: m.get_one::<usize>("length").cloned(),
-          algorithm_type: m.get_one::<String>("type").cloned(),
-          list_all: m.is_present("all"),
-          hash: m.get_one::<String>("hash").cloned(),
-      }
-  }
+    fn from(m: ArgMatches) -> Self {
+        exit_if_empty(&m);
+        Config {
+            length: m.get_one::<usize>("length").cloned(),
+            algorithm_type: m.get_one::<String>("type").cloned(),
+            list_all: m.is_present("all"),
+            hash: m.get_one::<String>("input").cloned(),
+        }
+    }
 }
 
 fn exit_if_empty(m: &ArgMatches) {
-  if !m.args_present() {
-      println!("Insufficient arguments, (-h, --help for options)");
-      std::process::exit(1);
-  }
+    if !m.args_present() {
+        println!("Insufficient arguments, (-h, --help for options)");
+        std::process::exit(1);
+    }
 }
