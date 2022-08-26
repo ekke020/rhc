5687a420de22fc3c1fc6d10e8e12da636b6ca9d6
Johan Ekman <ekman-johan020@hotmail.com>
2022-08-26 22:16:18 +0200

    Refactored the run_crack function, should be easier to follow now

--- src/systems/cracker.rs
+++ src/systems/cracker.rs
@@ -7,84 +7,77 @@ const ASCII_TABLE: [char; 95] = [
     'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
     'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
 ];
-
-pub fn crack(length: usize, start_char: &char) -> Option<PasswordInfo> {
-    run_crack(length, &start_char.to_string(), None)
+#[derive(Clone)]
+pub struct Cracked {
+    pw_length: usize,
+    pw_string: String,
+    hash_type: HashType,
 }
 
-fn run_crack(
+impl Cracked {
+    pub fn new(pw_length: usize, pw_string: String, hash_type: HashType) -> Self {
+        Self {
+            pw_length,
+            pw_string,
+            hash_type,
+        }
+    }
+
+    pub fn print(&self) {
+        println!("Hash type: {}", self.hash_type);
+        println!("Hash: {}", self.hash_type.get_hash());
+        println!("Password: {}", self.pw_string);
+        println!("Length: {}", self.pw_length);
+    }
+}
+
+pub fn run_crack(
+    algorithms: &Vec<HashType>,
     pw_length: usize,
     prefix: &String,
-    is_cracked: Option<PasswordInfo>,
-) -> Option<PasswordInfo> {
+    is_cracked: Option<Cracked>,
+) -> Option<Cracked> {
     if is_cracked.is_some() {
         return is_cracked;
     } else if pw_length == 0 {
-        return MATCHER_INFO.try_password(prefix);
+        return algorithms
+            .iter()
+            .find_map(|hash_type| try_password(prefix, hash_type));
     }
     for char in ASCII_TABLE {
         let mut temp = prefix.clone();
         temp.push(char);
-        if let Some(cracked) = run_crack(pw_length - 1, &temp, is_cracked.clone()) {
+        if let Some(cracked) = run_crack(algorithms, pw_length - 1, &temp, is_cracked.clone()) {
             return Some(cracked);
         }
     }
     None
 }
 
+fn try_password(password: &str, hash_type: &HashType) -> Option<Cracked> {
+    if hash_type.is_match(password) {
+        let cracked = Cracked::new(password.len(), password.to_string(), hash_type.clone());
+        return Some(cracked);
+    }
+    None
+}
 
-// pub fn testing() {
-//     let mut password_length = 1;
-//     let mut crack_result = None;
-//     while let None = crack_result {
-//         let test = print_all(&mut String::new(), ASCII_TABLE.len(), password_length, None);
-//         match test {
-//             Some(t) => {
-//                 crack_result = Some(t);
-//             }
-//             None => password_length += 1,
-//         }
-//     }
-//     println!("{}", crack_result.unwrap().to_string());
-// }
+#[cfg(test)]
+mod tests {
+    use crate::cracker;
+    use crate::systems::hasher::HashType;
 
-// fn print_all(
-//     prefix: &mut String,
-//     n: usize,
-//     k: i32,
-//     is_cracked: Option<PW::PasswordInfo>,
-// ) -> Option<PW::PasswordInfo> {
-//     if is_cracked.is_some() {
-//         println!("Hello");
-//         return is_cracked;
-//     }
-//     if k == 0 {
-//         return MATCHER_INFO.try_password(prefix);
-//     }
-//     let mut cracked = None;
-//     for i in 0..n {
-//         let char = ASCII_TABLE[i];
-//         let mut temp = prefix.clone();
-//         temp.push(char);
-//         cracked = print_all(&mut temp, n, k - 1, is_cracked.clone());
-//         if cracked.is_some() {
-//             return cracked;
-//         }
-//     }
-//     cracked
-// }
-
-// fn get_ascii() -> [u8; 95] {
-//     let ascii_num = macros::lazy_array!(32, 95, u8);
-//     let ascii_num_extended_one = macros::lazy_array!(161, 12, u8);
-//     let ascii_num_extended_two = macros::lazy_array!(174, 82, u8);
-//     let full_ascii: [u8; 189] = ascii_num
-//         .iter()
-//         .chain(&ascii_num_extended_one)
-//         .chain(&ascii_num_extended_two)
-//         .map(|code| *code)
-//         .collect::<Vec<u8>>()
-//         .try_into()
-//         .unwrap();
-//     ascii_num
-// }
+    #[test]
+    fn password_matches() {
+        let sha_224 =
+            HashType::Sha224("a7470858e79c282bc2f6adfd831b132672dfd1224c1e78cbf5bcd057".to_owned());
+        let should_be_some = cracker::try_password("12345", &sha_224);
+        assert!(should_be_some.is_some());
+    }
+    #[test]
+    fn password_does_not_match() {
+        let sha_512 = HashType::Sha512("3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79".to_owned());
+        let should_be_none = cracker::try_password("1234", &sha_512);
+        assert!(should_be_none.is_none());
+    }
+}
