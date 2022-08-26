c8aa45bb95b2fac8a35fe424426a353773f0dd17
Johan Ekman <ekman-johan020@hotmail.com>
2022-08-26 22:15:10 +0200

    Placed the password_info struct inside a Arc

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 mod config;
 mod input;
 mod systems;
-#[macro_use]
-extern crate lazy_static;
 pub mod prelude {
     pub use crate::config::Config;
     pub use crate::systems::cracker;
@@ -10,18 +8,14 @@ pub mod prelude {
     pub use crate::systems::hasher::HashType;
     pub use crate::systems::macros;
     pub use crate::systems::password_info::*;
-    pub use crate::systems::password_matcher as matcher;
     pub use crate::systems::printer::print;
     pub const CRACK_TEST: &str = "99fb2f48c6af4761f904fc85f95eb56190e5d40b1f44ec3a9c1fa319";
-    lazy_static! {
-        pub static ref MATCHER_INFO: matcher::MatcherInfo = matcher::MatcherInfo::new(CRACK_TEST);
-        pub static ref START_TIME: std::time::SystemTime = std::time::SystemTime::now();
-    }
 }
 use prelude::*;
+use std::sync::mpsc;
 use std::sync::mpsc::Sender;
+use std::sync::Arc;
 use std::thread;
-
 fn main() {
     let args = input::create_flag_matcher();
     let config = Config::from(args);
@@ -30,29 +24,35 @@ fn main() {
         .set_algorithm_type(config.get_algorithm_type())
         .set_password_length(config.get_length())
         .build();
-
-    // let (tx, rx) = mpsc::channel();
-    // for i in 0..95 {
-    //     let start_char = (32 + i as u8) as char;
-    //     spin_up_thread(tx.clone(), start_char, i);
-    // }
-    // let received = rx.recv().unwrap();
-    // received.print();
+    let pointer = Arc::new(pw_info);
+    let (tx, rx) = mpsc::channel();
+    for i in 0..95 {
+        let start_char = (32 + i as u8) as char;
+        spin_up_thread(tx.clone(), start_char, pointer.clone());
+    }
+    let received = rx.recv().unwrap();
+    received.print();
+    print::elapsed_time(pointer.get_elapsed_time());
 }
 
-fn spin_up_thread(tx: Sender<PasswordInfo>, star_char: char, num: i32) {
+fn spin_up_thread(
+    tx: Sender<cracker::Cracked>,
+    star_char: char,
+    pw_info_pointer: Arc<PasswordInfo>,
+) {
     thread::spawn(move || {
-        let mut pw_length = 0;
+        let mut pw_length = pw_info_pointer.get_password_length().clone();
+        let algorithms = pw_info_pointer.get_algorithms();
         loop {
-            println!("Thread: {} is on length: {}", num, pw_length + 1);
-            let password_info = cracker::crack(pw_length, &star_char);
-            match password_info {
+            let cracked =
+                cracker::run_crack(algorithms, pw_length, &char::to_string(&star_char), None);
+            match cracked {
                 Some(t) => {
                     tx.send(t).unwrap();
                     break;
                 }
                 None => pw_length += 1,
-            }
+            };
         }
     });
 }
