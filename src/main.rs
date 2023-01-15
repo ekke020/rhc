63f0d280b9ee47084e196339701dffa8419db722
Johan Ekman <ekman-johan020@hotmail.com>
2023-01-16 00:22:35 +0100

    Cleaned up

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,5 @@ mod sha2;
 mod systems;
 
 fn main() {
-    if let Err(e) = cli::entrypoint() {
-        println!("{}", e);
-        std::process::exit(e.get_exit_code())
-    }
+    let settings = cli::run();
 }
-
