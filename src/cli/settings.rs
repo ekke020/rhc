f4775dfba947785508d5c10c41a2c34c874b3462
Johan Ekman <johan.ekman@pricer.com>
2023-01-17 00:49:54 +0100

    Removed old and depricated code

--- src/cli/settings.rs
+++ src/cli/settings.rs
@@ -1,36 +1,33 @@
-use super::{
-    argument::describe,
-    error::flag::FlagError,
-    flag::{Flag, FlagInfo},
-};
-use std::collections::HashMap;
-
 pub enum Setting {
     HashInput(String),
     HashType(String),
     HashLenght(u32),
+    Verbose(bool),
 }
 
 pub struct GlobalSettings {
     hash_input: Option<Setting>,
     hash_type: Option<Setting>,
     hash_length: Option<Setting>,
+    verbose: Option<Setting>,
 }
 
 impl GlobalSettings {
-    fn new() -> Self {
+    pub(super) fn new() -> Self {
         GlobalSettings {
             hash_input: None,
             hash_type: None,
             hash_length: None,
+            verbose: None,
         }
     }
 
-    fn add_setting(&mut self, setting: Setting) {
+    pub fn add_setting(&mut self, setting: Setting) {
         match setting {
             Setting::HashInput(_) => self.hash_input = Some(setting),
             Setting::HashType(_) => self.hash_type = Some(setting),
             Setting::HashLenght(_) => self.hash_length = Some(setting),
+            Setting::Verbose(_) => self.verbose = Some(setting),
         }
     }
 
@@ -45,67 +42,8 @@ impl GlobalSettings {
     pub fn get_hash_length(&mut self) -> Option<Setting> {
         self.hash_length.take()
     }
-}
 
-pub fn produce_settings(flags: Vec<Flag>) -> Result<GlobalSettings, FlagError> {
-    let mut settings = GlobalSettings::new();
-    for mut flag in flags {
-        handle_flag(&mut flag, &mut settings)?;
+    pub fn is_verbose(&mut self) -> Option<Setting> {
+        self.verbose.take()
     }
-    Ok(settings)
-}
-
-fn handle_flag(flag: &mut Flag, settings: &mut GlobalSettings) -> Result<(), FlagError> {
-
-    // TODO: Simplify this function, its to repetitive atm.
-    match flag {
-        Flag::Input(info) => {
-            // TODO: Change the hardcoded value of "input" to the const value of argumentinfo
-            let input = info
-                .get_input()
-                .ok_or(FlagError::missing_input_error("input"))?;
-            settings.hash_input = Some(Setting::HashInput(input));
-        }
-        Flag::Type(info) => {
-            // TODO: Change the hardcoded value of "type" to the const value of argumentinfo
-            let input = info
-                .get_input()
-                .ok_or(FlagError::missing_input_error("type"))?;
-            settings.hash_type = Some(Setting::HashType(input));
-        }
-        Flag::Length(info) => {
-            // TODO: Change the hardcoded value of "length" to the const value of argumentinfo
-            let input = info
-                .get_input()
-                .ok_or(FlagError::missing_input_error("length"))?;
-            let test = input
-                .parse::<u32>()
-                .ok()
-                .ok_or(FlagError::invalid_input_type("length"))?;
-            settings.hash_length = Some(Setting::HashLenght(test));
-        }
-        Flag::Version(info) => {
-            info.get_help().ne(&true).then(|| version());
-        }
-        Flag::Help(info) => {
-            info.get_help().then(|| help());
-        }
-    };  
-    Ok(())
-}
-
-use super::argument::info::ARGUMENTS;
-fn help() {
-    println!("usage: rhc [OPTION]...");
-    ARGUMENTS
-        .iter()
-        .for_each(|arg| println!("{}", describe(arg)));
-    std::process::exit(0);
-}
-
-// TODO: Move this value to the correct place
-pub const VERSION: &str = "0.0.1";
-fn version() {
-    println!("Version: {}", VERSION);
-    std::process::exit(0);
 }
