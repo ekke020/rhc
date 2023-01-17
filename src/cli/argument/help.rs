7f69d7cd8c3a0e6eaa8581b49600e69bd4333f48
Johan Ekman <ekman-johan020@hotmail.com>
2023-01-17 20:00:06 +0100

    Changed the help function so its more dynamic

--- src/cli/argument/help.rs
+++ src/cli/argument/help.rs
@@ -1,7 +1,7 @@
 use crate::cli::flags::get_help;
 
-pub fn print_help() {
-    let f = get_help("--help").unwrap();
+pub fn print_help(flag: &str) {
+    let f = get_help(flag).unwrap();
     println!("{}", f.help());
     std::process::exit(0);
 }
--- src/cli/argument/mod.rs
+++ src/cli/argument/mod.rs
@@ -1,2 +1,11 @@
-pub mod help;
-pub mod version;
+mod help;
+mod version;
+
+pub fn help_and_exit(arg: Option<&str>) {
+    let flag = arg.unwrap_or("--help");
+    help::print_help(flag);
+}
+
+pub fn version_and_exit() {
+    version::print_version();
+}
