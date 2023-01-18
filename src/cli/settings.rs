eabd5fa921e1531885a45dba4b8655277baf63f2
Johan Ekman <ekman-johan020@hotmail.com>
2023-01-18 23:14:41 +0100

    Made some changes to how settings are stored

--- src/cli/settings.rs
+++ src/cli/settings.rs
@@ -6,10 +6,10 @@ pub enum Setting {
 }
 
 pub struct GlobalSettings {
-    hash_input: Option<Setting>,
-    hash_type: Option<Setting>,
-    hash_length: Option<Setting>,
-    verbose: Option<Setting>,
+    hash_input: Option<String>,
+    hash_type: Option<String>,
+    hash_length: Option<u32>,
+    verbose: bool,
 }
 
 impl GlobalSettings {
@@ -18,32 +18,32 @@ impl GlobalSettings {
             hash_input: None,
             hash_type: None,
             hash_length: None,
-            verbose: None,
+            verbose: false,
         }
     }
 
     pub fn add_setting(&mut self, setting: Setting) {
         match setting {
-            Setting::HashInput(_) => self.hash_input = Some(setting),
-            Setting::HashType(_) => self.hash_type = Some(setting),
-            Setting::HashLenght(_) => self.hash_length = Some(setting),
-            Setting::Verbose(_) => self.verbose = Some(setting),
+            Setting::HashInput(value) => self.hash_input = Some(value),
+            Setting::HashType(value) => self.hash_type = Some(value),
+            Setting::HashLenght(value) => self.hash_length = Some(value),
+            Setting::Verbose(value) => self.verbose = value,
         }
     }
 
-    pub fn get_hash_input(&mut self) -> Option<Setting> {
+    pub fn get_hash_input(&mut self) -> Option<String> {
         self.hash_input.take()
     }
 
-    pub fn get_hash_type(&mut self) -> Option<Setting> {
+    pub fn get_hash_type(&mut self) -> Option<String> {
         self.hash_type.take()
     }
 
-    pub fn get_hash_length(&mut self) -> Option<Setting> {
+    pub fn get_hash_length(&mut self) -> Option<u32> {
         self.hash_length.take()
     }
 
-    pub fn is_verbose(&mut self) -> Option<Setting> {
-        self.verbose.take()
+    pub fn is_verbose(&self) -> bool {
+        self.verbose
     }
 }
