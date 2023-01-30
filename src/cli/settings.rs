e65dcab64856c66ef1ced442aa67ef47a367719f
Johan Ekman <ekman-johan020@hotmail.com>
2023-01-30 01:15:40 +0100

    Added a AlgorithmType enum

--- src/cli/settings.rs
+++ src/cli/settings.rs
@@ -1,13 +1,41 @@
-pub enum Setting {
-    HashInput(String),
-    HashType(String),
-    HashLenght(u32),
-    Verbose(bool),
+use crate::core::setup::Package;
+
+use super::error::argument::ArgumentError;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum AlgorithmType {
+    Sha2_224,
+    Sha2_256,
+    Sha2_384,
+    Sha2_512,
+    Sha2_512_224,
+    Sha2_512_256,
 }
 
+impl AlgorithmType {
+    pub fn from(value: &str) -> Result<Self, ArgumentError> {
+        match value {
+            "sha2_224" => Ok(AlgorithmType::Sha2_224),
+            "sha2_256" => Ok(AlgorithmType::Sha2_256), 
+            "sha2_384" => Ok(AlgorithmType::Sha2_384),
+            "sha2_512" => Ok(AlgorithmType::Sha2_512), 
+            "sha2_512_224" => Ok(AlgorithmType::Sha2_512_224), 
+            "sha2_512_256" => Ok(AlgorithmType::Sha2_512_256),
+            _ => Err(ArgumentError::unsupported_algorithm(value))
+        }
+    }
+}
+
+pub enum Setting {
+    HashInput(String),
+    HashType(AlgorithmType),
+    HashLength(u32),
+    Verbose(bool),
+}
+#[derive(Debug, Clone)]
 pub struct GlobalSettings {
     hash_input: Option<String>,
-    hash_type: Option<String>,
+    hash_type: Option<AlgorithmType>,
     hash_length: Option<u32>,
     verbose: bool,
 }
@@ -26,7 +54,7 @@ impl GlobalSettings {
         match setting {
             Setting::HashInput(value) => self.hash_input = Some(value),
             Setting::HashType(value) => self.hash_type = Some(value),
-            Setting::HashLenght(value) => self.hash_length = Some(value),
+            Setting::HashLength(value) => self.hash_length = Some(value),
             Setting::Verbose(value) => self.verbose = value,
         }
     }
@@ -35,7 +63,7 @@ impl GlobalSettings {
         self.hash_input.take()
     }
 
-    pub fn get_hash_type(&mut self) -> Option<String> {
+    pub fn get_hash_type(&mut self) -> Option<AlgorithmType> {
         self.hash_type.take()
     }
 
