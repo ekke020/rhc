72e94506bbaf3df896e4a17807e8c7ed72f59cb4
Johan Ekman <ekman-johan020@hotmail.com>
2023-01-30 01:15:58 +0100

    Added more errors

--- src/core/error/core.rs
+++ src/core/error/core.rs
@@ -7,21 +7,34 @@ const NOT_YET_SPECIFIED: i32 = 0x40;
 #[derive(Debug, PartialEq)]
 enum CoreErrorKind {
     NotYetSpecified,
+    InvalidAlgorithm,
+    DetermineAlgorithm(String),
+    MissingHashInput,
 }
 
 impl CoreErrorKind {
     fn get_exit_code(&self) -> i32 {
         match self {
             CoreErrorKind::NotYetSpecified => NOT_YET_SPECIFIED,
+            CoreErrorKind::InvalidAlgorithm => NOT_YET_SPECIFIED,
+            CoreErrorKind::MissingHashInput => NOT_YET_SPECIFIED,
+            CoreErrorKind::DetermineAlgorithm(_) => NOT_YET_SPECIFIED,
         }
     }
 
     fn get_error_message(&self) -> String {
         match self {
             CoreErrorKind::NotYetSpecified => String::new(),
+            CoreErrorKind::InvalidAlgorithm => String::from("Invalid algorithm supplied\nsee --algorithm --help for available options."),
+            CoreErrorKind::MissingHashInput => String::from("No hash supplied, unable to run\nsee --password --help for information"),
+            CoreErrorKind::DetermineAlgorithm(v) => format!("Unable to determine a possible algorithm from: {v}\nsee --algorithm --help for available options"),
         }
     }
 }
+
+pub const MISSING_HASH_INPUT_ERROR: CoreError = CoreError(CoreErrorKind::MissingHashInput);
+pub const INVALID_ALGORITHM_ERROR: CoreError = CoreError(CoreErrorKind::InvalidAlgorithm);
+// pub const DETERMINE_ALGORITHM_ERROR: CoreError = CoreError(CoreErrorKind::DetermineAlgorithm);
 
 #[derive(Debug, PartialEq)]
 pub struct CoreError(CoreErrorKind);
@@ -30,6 +43,11 @@ impl CoreError {
 
     pub fn get_exit_code(&self) -> i32 {
         self.0.get_exit_code()
+    }
+
+    pub fn determine_algorithm(value: &Vec<u8>) -> Self {
+        let input = std::str::from_utf8(value).unwrap();
+        CoreError(CoreErrorKind::DetermineAlgorithm(input.to_owned()))
     }
 }
 
