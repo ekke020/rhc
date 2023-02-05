738eb90b730a326b0549fcdfac62a398d89f57f3
Johan Ekman <ekman-johan020@hotmail.com>
2023-02-05 16:25:42 +0100

    Added a result struct for displaying a successful crack

---
+++ src/core/crack/result.rs
@@ -0,0 +1,36 @@
+use std::fmt::Display;
+
+pub struct PasswordMatch {
+    password: String,
+    algorithm: String,
+    length: usize,
+    target: Vec<u8>,
+}
+
+impl PasswordMatch {
+    pub fn from(password: String, algorithm: String, target: Vec<u8>) -> Self {
+        Self {
+            length: password.len(),
+            password,
+            algorithm,
+            target,
+        }
+    }
+}
+
+impl Display for PasswordMatch {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Found a match for: {}\nMatch: {}\nLength: {}\nAlgorithm: {}",
+            target_to_lower_hex(&self.target),
+            self.password,
+            self.length,
+            self.algorithm
+        )
+    }
+}
+
+fn target_to_lower_hex(target: &Vec<u8>) -> String {
+    target.iter().map(|dec| format!("{:X}", dec)).collect()
+}
