f5486f2c02d1f8556eaf02846c50a0e9745980e6
Johan Ekman <ekman-johan020@hotmail.com>
2023-02-05 16:26:50 +0100

    Added a temporary solution to running the program

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -11,8 +11,20 @@ use crate::cli::settings::GlobalSettings;
 pub type Error = error::core::CoreError;
 
 // TODO: Return a result of the successful crack.
-pub fn run(mut settings: GlobalSettings) -> Result<(), CoreError>{
-    let package = Package::assemble(settings)?;
+pub fn run(mut settings: GlobalSettings) -> Result<(), CoreError> {
+    let package = Package::assemble(&mut settings)?;
+    let wordlist = settings.get_wordlist();
 
+    let threads = match wordlist {
+        Some(wordlist) => spawn::resource_job(package, wordlist)?,
+        None => spawn::brute_force_job(package)?,
+    };
+    for thread in threads {
+        let result = thread.join().unwrap();
+        if result.is_some() {
+            println!("{}", result.unwrap());
+            break;
+        }
+    }
     Ok(())
 }
