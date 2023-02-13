799e0f30375dedd9fac0f688816f3e2a03586e3f
Johan Ekman <johan.ekman@pricer.com>
2023-02-13 15:59:43 +0100

    Added the bruteforce strategy as a POC

--- src/core/crack/bruteforce.rs
+++ src/core/crack/bruteforce.rs
@@ -6,55 +6,72 @@ use std::{
     },
 };
 
-use crate::core::{package::Package, constants::ASCII_95_TABLE};
+use crate::{
+    algorithm::{self, Algorithm},
+    core::{constants::ASCII_95_TABLE, package::Package},
+};
 
-pub(super) struct BruteForce {
-    start: usize,
-    end: usize,
+use super::result::PasswordMatch;
+
+pub struct BruteForce<'a> {
+    target: &'a Vec<u8>,
+    range: &'static [u32],
     counter: Arc<AtomicU32>,
-    package: Package,
+    algorithm: Box<dyn Algorithm>,
 }
 
-impl BruteForce {
-    pub fn from(ascii_span: (usize, usize), counter: Arc<AtomicU32>, package: Package) -> Self {
-        Self { 
-            start: ascii_span.0,
-            end: ascii_span.1,
+impl<'a> BruteForce<'a> {
+    pub fn from(
+        target: &'a Vec<u8>,
+        range: &'static [u32],
+        counter: Arc<AtomicU32>,
+        algorithm: Box<dyn Algorithm>,
+    ) -> Self {
+        Self {
+            target,
+            range,
             counter,
-            package,
-         }
-    }
-
-    pub fn run(&self) {
-        let chunk = &ASCII_95_TABLE[self.start..self.end];
-        for c in chunk {
-            let char = from_u32(*c).unwrap();
-            run_crack(5, char.to_string(), &mut false, &self.counter);
+            algorithm,
         }
     }
+    pub fn run(&mut self) -> Option<PasswordMatch> {
+        for c in self.range {
+            let char = from_u32(*c).unwrap();
+            crack(self, None, 4,char.to_string());
+        }
+        println!("Done");
+        None
+    }
 }
 
-pub fn run_crack(length: usize, mut word: String, is_cracked: &mut bool, count: &Arc<AtomicU32>) {
-    if *is_cracked {
-        return;
+fn crack(
+    bf: &mut BruteForce,
+    cracked: Option<String>,
+    length: usize,
+    mut word: String,
+) -> Option<String> {
+    if cracked.is_some() {
+        return cracked;
     } else if length == 0 {
-        let total = count.fetch_add(1, Ordering::SeqCst);
-        test_print(&word, total);
-        return;
+        let total = bf.counter.fetch_add(1, Ordering::SeqCst);
+        let test = algorithm::execute_comparison(bf.algorithm.as_mut(), &word, bf.target);
+        if test {
+            println!("got em: {}", word);
+            std::process::exit(0);
+        }
+        if total % 10000000 == 0 {
+            println!("I have hashed: {} words", total);
+            println!("Current word: {}", word);
+            println!("Current length: {}", word.len());
+        }
+        return None;
     }
 
     ASCII_95_TABLE.iter().for_each(|c| {
         word.push(from_u32(*c).unwrap());
-        run_crack(length - 1, word.clone(), is_cracked, count);
+        crack(bf, None, length - 1, word.clone());
         word.pop();
     });
+    None
 }
 
-fn test_print(word: &str, count: u32) {
-    let mut sha = crate::sha2::Sha224::from(word);
-    sha.run();
-    let t = sha.extract();
-    if count % 1000000 == 0 {
-        println!("I have hashed: {} words", count);
-    }
-}
