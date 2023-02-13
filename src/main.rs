29d46950fbc3f8cb6ef4af28dbf502566e859251
Johan Ekman <johan.ekman@pricer.com>
2023-02-13 16:15:57 +0100

    Removed the timer

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,5 @@ mod sha2;
 use std::time::{Duration, Instant};
 
 fn main() {
-    let start = Instant::now();
     rhc::run().unwrap_or_else(|e| e.exit());
-    let duration = start.elapsed();
-    elapsed_time(duration.as_secs());
-}
-
-fn elapsed_time(elapsed: u64) {
-    let seconds = (elapsed % 3600) % 60;
-    let minutes = (elapsed % 3600 - seconds) / 60;
-    let hours = (elapsed - minutes * 60 + seconds) / 3600;
-    println!("H: {}, M: {}, S: {}", hours, minutes, seconds);
 }
