5ca02fa674796674855823999c21ac09e4df8c0b
Johan Ekman <ekman-johan020@hotmail.com>
2023-02-14 22:15:07 +0100

    Optimized a fair bit

--- src/core/crack/bruteforce.rs
+++ src/core/crack/bruteforce.rs
@@ -4,84 +4,130 @@ use std::{
         atomic::{AtomicU32, Ordering},
         Arc,
     },
+    time::Instant,
 };
 
 use crate::{
     algorithm::{self, Algorithm},
-    core::{constants::ASCII_95_TABLE, package::Package},
+    core::{
+        constants::{ASCII_95_TABLE, NO_SPECIAL_RANGE},
+        package::Package,
+    },
 };
 
 use super::result::PasswordMatch;
 
 pub struct BruteForce<'a> {
     target: &'a Vec<u8>,
-    range: &'static [u32],
+    range: &'static [u8],
     counter: Arc<AtomicU32>,
-    instant: Arc<std::time::Instant>,
+    instant: Instant,
     algorithm: Box<dyn Algorithm>,
 }
 
 impl<'a> BruteForce<'a> {
     pub fn from(
         target: &'a Vec<u8>,
-        range: &'static [u32],
+        range: &'static [u8],
         counter: Arc<AtomicU32>,
-        instant: Arc<std::time::Instant>,
         algorithm: Box<dyn Algorithm>,
     ) -> Self {
         Self {
             target,
             range,
             counter,
-            instant,
+            instant: Instant::now(),
             algorithm,
         }
     }
     pub fn run(&mut self) -> Option<PasswordMatch> {
-        for c in self.range {
-            let char = from_u32(*c).unwrap();
-            crack(self, None, 4,char.to_string());
+        let mut n = 4;
+        let pm = 'runner: loop {
+            for c in self.range {
+                let mut word = Vec::with_capacity(n);
+                word.push(*c);
+                if let Some(result) = self.calculate(None, n - 1, &mut word) {
+                    break 'runner result;
+                }
+            }
+            n += 1;
+        };
+        Some(pm)
+    }
+
+    fn calculate(
+        &mut self,
+        pm: Option<PasswordMatch>,
+        length: usize,
+        word: &mut Vec<u8>,
+    ) -> Option<PasswordMatch> {
+        if pm.is_some() {
+            return pm;
+        } else if length == 0 {
+            let algorithm = self.algorithm.as_mut();
+            algorithm.populate(word.as_slice());
+            algorithm.execute();
+            algorithm.compare(self.target);
+            PasswordMatch::from(
+                password.to_string(),
+                self.algorithm.to_string(),
+                self.target.to_vec(),
+            );
+            return None;
         }
+        NO_SPECIAL_RANGE.iter().for_each(|c| {
+            word.push(*c);
+            self.calculate(None, length - 1, word);
+            word.pop();
+        });
         None
+    }
+
+    fn execute_comparison(&self, word: Vec<u8>) -> Option<PasswordMatch> {
+        let algorithm = self.algorithm.as_mut();
+        algorithm.populate(word.as_slice());
+        algorithm.execute();
+        match algorithm.compare(self.target) {
+            true => self.create_password_match(),
+            false => None,
+        }
+    }
+
+    fn create_password_match(&self) -> PasswordMatch {
+        PasswordMatch::from(
+            password.to_string(),
+            self.algorithm.to_string(),
+            self.target.to_vec(),
+        )
     }
 }
 
-fn crack(
+fn calculate(
     bf: &mut BruteForce,
-    cracked: Option<String>,
+    pm: Option<PasswordMatch>,
     length: usize,
-    mut word: String,
-) -> Option<String> {
-    if cracked.is_some() {
-        return cracked;
+    word: &mut Vec<u8>,
+) -> Option<PasswordMatch> {
+    if pm.is_some() {
+        return pm;
     } else if length == 0 {
-        let total = bf.counter.fetch_add(1, Ordering::SeqCst);
-        let test = algorithm::execute_comparison(bf.algorithm.as_mut(), &word, bf.target);
-        if test {
-            println!("got em: {}", word);
-            elapsed_time(bf.instant.elapsed().as_secs());
-            std::process::exit(0);
-        }
-        if total % 10000000 == 0 {
-            println!("I have hashed: {} words", total);
-            println!("Current word: {}", word);
-            println!("Current length: {}", word.len());
-        }
         return None;
     }
-
-    ASCII_95_TABLE.iter().for_each(|c| {
-        word.push(from_u32(*c).unwrap());
-        crack(bf, None, length - 1, word.clone());
+    NO_SPECIAL_RANGE.iter().for_each(|c| {
+        word.push(*c);
+        calculate(bf, None, length - 1, word);
         word.pop();
     });
     None
 }
 
 
-fn elapsed_time(elapsed: u64) {
-    let seconds = (elapsed % 3600) % 60;
-    let minutes = (elapsed % 3600 - seconds) / 60;
-    let hours = (elapsed - minutes * 60 + seconds) / 3600;
-    println!("H: {}, M: {}, S: {}", hours, minutes, seconds);
+fn execute_comparison(bf: &BruteForce, word: Vec<u8>) -> Option<PasswordMatch> {
+    let algorithm = bf.algorithm.as_mut();
+    algorithm.populate(word.as_slice());
+    algorithm.execute();
+    match algorithm.compare(bf.target) {
+        true => todo!(),
+        false => None,
+    }
 }
