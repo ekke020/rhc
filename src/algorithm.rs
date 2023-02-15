2cac00fc0e6bf57e793302f2d8ab30fe168306ab
Johan Ekman <ekman-johan020@hotmail.com>
2023-02-15 21:29:26 +0100

    Removed old test

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -138,16 +138,3 @@ impl Algorithm for Sha512_256 {
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::{execute_comparison, AlgorithmType};
-    #[test]
-    fn test_execute_comparison() {
-        let target = vec![
-            219, 60, 218, 134, 212, 66, 154, 29, 57, 193, 72, 152, 149, 102, 179, 143, 123, 218, 1,
-            86, 41, 107, 211, 100, 186, 47, 135, 139,
-        ];
-        let mut algorithm = AlgorithmType::from("sha2_224").unwrap().get_algorithm();
-        assert!(execute_comparison(algorithm.as_mut(), "ab".as_bytes(), &target));
-    }
-}
