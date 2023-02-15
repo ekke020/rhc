3823db6899fd5e2b6cd4833ba56ecfe773262be4
Johan Ekman <ekman-johan020@hotmail.com>
2023-02-15 21:38:02 +0100

    Cleaned up a bit

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,5 @@ mod sha2;
 use std::time::{Duration, Instant};
 
 fn main() {
-    // let instant = std::time::Instant::now();
     rhc::run().unwrap_or_else(|e| e.exit());
-    // println!("{}", instant.elapsed().as_millis());
-    // NO_SPECIAL_RANGE.iter().for_each(|c| print!("0x{:x}, ", *c as i32));
-}
-
-fn elapsed_time(elapsed: u64) {
-    let seconds = (elapsed % 3600) % 60;
-    let minutes = (elapsed % 3600 - seconds) / 60;
-    let hours = (elapsed - minutes * 60 + seconds) / 3600;
-    println!("H: {}, M: {}, S: {}", hours, minutes, seconds);
 }
