75d6235bd692c0e2b55d969420420d26cb141520
Johan Ekman <johan.ekman@pricer.com>
2023-02-15 11:13:57 +0100

    Added some error handling and better output

--- scripts/concat_lists.py
+++ scripts/concat_lists.py
@@ -7,22 +7,31 @@ def load_values(filepath):
         content.append(line.strip())
   return set(content)
 
-def write_values(combined):
-  with open('test.txt', 'w') as file:
-    for password in combined:
-      file.write(password + '\n')
-
 try:
   file_1 = sys.argv[1]
   file_2 = sys.argv[2]
 except:
-   print("Missing arguments ")
+   print(f'Requires 2 arguments but only {len(sys.argv) - 1} was provided')
+   exit(1)
 
 file_1_content = []
 
-set_1 = load_values(file_1)
-set_2 = load_values(file_2)
+try:
+  set_1 = load_values(file_1)
+  set_2 = load_values(file_2)
+except FileNotFoundError as e:
+   print(f'No file at given path: {e.filename}')
+   exit(1)
 
 combined = set_1.union(set_2)
-write_values(combined)
+write_file = 'output.txt'
+try:
+  with open(write_file, 'w') as file:
+    file.write(combined.pop())
+    for password in combined:
+      file.write(f'\n{password}')
+except:
+  print(f'Unable to write to file: {write_file}')
+  exit(1)
 
+print(f'Successfully combined {file_1} with {file_2} and produced {write_file}')
