6cffda8d77e93cb2e88949dd77eb2ef724f43278
Johan Ekman <ekman-johan020@hotmail.com>
2023-02-15 21:35:33 +0100

    Still a work in progress

--- src/core/crack/wrapper.rs
+++ src/core/crack/wrapper.rs
@@ -3,7 +3,7 @@ use crate::core::package::Package;
 use super::result::PasswordMatch;
 
 pub trait Crack {
-    fn from(package: &Package) -> Self;
+    fn from(package: Package, index: usize) -> Self;
     fn run(&mut self) -> Option<PasswordMatch>;
 }
 
@@ -11,12 +11,17 @@ pub struct Wrapper<T> {
     crack: T,
 }
 
-impl<T> Wrapper<T> where T: Crack {
-    pub fn un_pack(package: &Package) -> T {
-        T::from(package)
+impl<T> Wrapper<T>
+where
+    T: Crack,
+{
+    pub fn un_pack(package: Package, index: usize) -> Self {
+        Self {
+            crack: T::from(package, index),
+        }
     }
 
     pub fn run(&mut self) -> Option<PasswordMatch> {
-        self.run()
+        self.crack.run()
     }
 }
