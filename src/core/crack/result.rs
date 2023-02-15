19b1f4081e72dbc180de216cfe382b47fc6b60a3
Johan Ekman <ekman-johan020@hotmail.com>
2023-02-15 21:35:01 +0100

    Result will now show elapsed time

--- src/core/crack/result.rs
+++ src/core/crack/result.rs
@@ -1,36 +1,47 @@
-use std::fmt::Display;
+use std::{fmt::Display, str::from_utf8};
 // TODO: Iclude an optional Instant if timers were enabled.
 pub struct PasswordMatch {
     password: String,
     algorithm: String,
     length: usize,
-    target: Vec<u8>,
+    target: String,
+    elapsed: u64,
 }
 
 impl PasswordMatch {
-    pub fn from(password: String, algorithm: String, target: Vec<u8>) -> Self {
+    pub fn from(password: &str, algorithm: String, target: &Vec<u8>, elapsed: u64) -> Self {
         Self {
             length: password.len(),
-            password,
+            password: password.to_owned(),
             algorithm,
-            target,
+            target: target_to_lower_hex(target),
+            elapsed
         }
     }
 }
 
 impl Display for PasswordMatch {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let elapsed = elapsed_time(self.elapsed);
         write!(
             f,
-            "Found a match for: {}\nMatch: {}\nLength: {}\nAlgorithm: {}",
-            target_to_lower_hex(&self.target),
+            "Found a match for: {}\nMatch: {}\nLength: {}\nAlgorithm: {}\n{}",
+            self.target,
             self.password,
             self.length,
-            self.algorithm
+            self.algorithm,
+            elapsed
         )
     }
 }
 
 fn target_to_lower_hex(target: &Vec<u8>) -> String {
     target.iter().map(|dec| format!("{:X}", dec)).collect()
+}
+
+fn elapsed_time(elapsed: u64) -> String {
+    let seconds = (elapsed % 3600) % 60;
+    let minutes = (elapsed % 3600 - seconds) / 60;
+    let hours = (elapsed - minutes * 60 + seconds) / 3600;
+    format!("H: {}, M: {}, S: {}", hours, minutes, seconds)
 }
