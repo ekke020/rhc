2c5bd217acd106568b09622cbf4fea0b63f1c6cb
Johan Ekman <ekman-johan020@hotmail.com>
2023-02-15 21:34:02 +0100

    Removed a test for now

--- src/core/crack/mod.rs
+++ src/core/crack/mod.rs
@@ -1,5 +1,6 @@
-pub(super) mod dictionary;
-pub(super) mod incremental;
+pub mod consts;
+mod dictionary;
+mod incremental;
 pub mod mode;
 pub mod result;
 mod wrapper;
@@ -8,34 +9,25 @@ use self::wrapper::Wrapper;
 use dictionary::Dictionary as DictionaryCore;
 use incremental::Incremental as IncrementalCore;
 
-pub type Incremental<'a> = Wrapper<IncrementalCore<'a>>;
+pub type Incremental = Wrapper<IncrementalCore>;
 pub type Dictionary<'a> = Wrapper<DictionaryCore<'a>>;
 
-fn calculate(
-    bf: &mut IncrementalCore,
-    pm: Option<result::PasswordMatch>,
-    length: usize,
-    word: &mut Vec<u8>,
-) -> Option<result::PasswordMatch> {
-    if pm.is_some() {
-        return pm;
-    } else if length == 0 {
-        return None;
-    }
-    super::constants::NO_SPECIAL_RANGE.iter().for_each(|c| {
-        word.push(*c);
-        calculate(bf, None, length - 1, word);
-        word.pop();
-    });
-    None
-}
 
-pub fn execute_comparison(
-    algorithm: &mut dyn crate::algorithm::Algorithm,
-    word: &[u8],
-    target: &Vec<u8>,
-) -> bool {
-    algorithm.populate(word);
-    algorithm.execute();
-    algorithm.compare(target)
-}
+// #[cfg(test)]
+// mod test {
+//     use crate::algorithm::AlgorithmType;
+
+//     #[test]
+//     fn test_execute_comparison() {
+//         let target = vec![
+//             219, 60, 218, 134, 212, 66, 154, 29, 57, 193, 72, 152, 149, 102, 179, 143, 123, 218, 1,
+//             86, 41, 107, 211, 100, 186, 47, 135, 139,
+//         ];
+//         let mut algorithm = AlgorithmType::from("sha2_224").unwrap().get_algorithm();
+//         assert!(execute_comparison(
+//             algorithm.as_mut(),
+//             "ab".as_bytes(),
+//             &target
+//         ));
+//     }
+// }
