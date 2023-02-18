de05f16d94eb468726f3fa68e86f3ded687b1294
Johan Ekman <ekman-johan020@hotmail.com>
2023-02-18 09:30:31 +0100

    Tweaked a few things and added some todos

--- src/cli/settings.rs
+++ src/cli/settings.rs
@@ -1,38 +1,39 @@
 use crate::{algorithm::AlgorithmType, core::crack::mode::Mode};
 
 use super::error::argument::ArgumentError;
-
+use std::collections::HashSet;
 pub enum Setting {
-    HashInput(String),
+    HashInput(Vec<u8>),
     HashType(AlgorithmType),
     HashLength(u32),
     Verbose(bool),
     Wordlist(Vec<String>),
     Mode(Mode),
 }
-// TODO: Rethink the Mode here, maybe a list of modes to go for? Where Mode::Incremental is added by default.
+// TODO: Might want to rename the variables in the Struct.
+// TODO: Look over missing memebers of the Struct.
+// TODO: Consider changing hash_input to be a list, this would require a lot of work...
 #[derive(Debug, Clone)]
 pub struct GlobalSettings {
-    hash_input: Option<String>,
+    hash_input: Option<Vec<u8>>,
     hash_type: Option<AlgorithmType>,
     hash_length: Option<u32>,
     wordlist: Option<Vec<String>>,
     verbose: bool,
-    mode: Mode,
+    modes: HashSet<Mode>,
 }
-// TODO: Change visibility to super 
+
 impl GlobalSettings {
-    pub(crate) fn new() -> Self {
+    pub(super) fn new() -> Self {
         GlobalSettings {
             hash_input: None,
             hash_type: None,
             hash_length: None,
             wordlist: None,
             verbose: false,
-            mode: Mode::Incremental
+            modes: HashSet::from([Mode::Incremental]),
         }
     }
-
     pub fn add_setting(&mut self, setting: Setting) {
         match setting {
             Setting::HashInput(value) => self.hash_input = Some(value),
@@ -40,11 +41,11 @@ impl GlobalSettings {
             Setting::HashLength(value) => self.hash_length = Some(value),
             Setting::Verbose(value) => self.verbose = value,
             Setting::Wordlist(value) => self.wordlist = Some(value),
-            Setting::Mode(mode) => self.mode = mode,
+            Setting::Mode(mode) => {self.modes.insert(mode);},
         }
     }
 
-    pub fn get_hash_input(&mut self) -> Option<String> {
+    pub fn get_hash_input(&mut self) -> Option<Vec<u8>> {
         self.hash_input.take()
     }
 
@@ -64,7 +65,7 @@ impl GlobalSettings {
         self.verbose
     }
 
-    pub fn get_mode(&self) -> &Mode {
-        &self.mode
+    pub fn is_mode(&self, mode: Mode) -> bool {
+        self.modes.contains(&mode)
     }
 }
