390ef34509b56be1a42ff77cf7bf8bc41bd5676b
Johan Ekman <ekman-johan020@hotmail.com>
2023-02-18 09:32:21 +0100

    Wrote a plan

---
+++ src/core/setup.rs
@@ -0,0 +1,6 @@
+// TODO: Replace package with a SettingsValidator 
+// TODO: After validating, prepare settings for respective thread.
+// TODO: ThreadSettings? Include symbols slice?
+// TODO: Return an array containing the prepared settings and distribute across threads.
+// TODO: Tear down the threads after a sucessfull crack
+// TODO: Present output 
