d278983bba1fd4062268469a7dcf15d54ca782e1
Johan Ekman <ekman-johan020@hotmail.com>
2023-03-19 23:12:44 +0100

    Added TODOS

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -6,3 +6,6 @@ pub mod result;
 use self::error::core::CoreError;
 
 pub type Error = error::core::CoreError;
+
+// TODO: The dictionary and incremental modes share a lot of similarities
+// TODO: Should introduce some polymorphism here.
