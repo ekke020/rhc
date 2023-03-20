34aabe850dd0abe7434c99f598ca25f0195edc12
Johan Ekman <johan.ekman@pricer.com>
2023-03-20 17:21:09 +0100

    Added support for word_size increase and a added some temporary output when finding a match

--- src/central/mod.rs
+++ src/central/mod.rs
@@ -20,6 +20,7 @@ pub enum Message {
 }
 
 pub struct Manager {
+    thread_count: usize,
     current_word_size: usize,
     word_size_counter: usize,
     processed_word_count: usize,
@@ -29,8 +30,11 @@ pub struct Manager {
 }
 
 impl Manager {
-    pub fn new() -> Self {
-        Self::default()
+    pub fn new(thread_count: usize) -> Self {
+        Self {
+            thread_count,
+            ..Self::default()
+        }
     }
 
     pub fn initialize(&self, thread_settings: Vec<ThreadSettings>) {
@@ -39,17 +43,24 @@ impl Manager {
             .for_each(|settings| spawn::job(self.tx.clone(), settings));
     }
 
-    pub fn listen(&mut self, is_quiet: bool) {
+    pub fn listen(&mut self) {
         // TODO: Implement methods for the different messages.
         // TODO: Figure out a way to handle quiet mode.
-        loop {
+        'listener: loop {
             if let Ok(message) = self.rx.recv() {
                 match message {
-                    Message::DictionaryProcessed => self.processed_dictionary_count += 1,
-                    Message::WordSizeIncreased => self.word_size_counter += 1,
-                    Message::ProcessedWordCount(count) => self.processed_word_count += count,
-                    Message::PasswordMatch(p_match) => todo!(),
-                    Message::NoMatch => todo!(),
+                    Message::DictionaryProcessed => self.handle_dictionary_processed(),
+                    Message::WordSizeIncreased => self.handle_word_size_increase(),
+                    Message::ProcessedWordCount(count) => self.handle_processed_word_count(),
+                    Message::PasswordMatch(password_match) => {
+                        self.handle_password_match(password_match);
+                        break 'listener;
+                    },
+                    Message::NoMatch => {
+                        // TODO: Maybe change listen to return a result<(), CoreError> instead?
+                        // TODO: Then we can throw an error if all threads have returned NoMatch.
+                        todo!()
+                    },
                 }
             } else {
                 todo!("Handle the error");
@@ -57,13 +68,37 @@ impl Manager {
             }
         }
     }
+
+    fn handle_dictionary_processed(&mut self) {
+        self.processed_dictionary_count += 1
+    }
+
+    fn handle_word_size_increase(&mut self) {
+        self.word_size_counter += 1;
+        if self.word_size_counter == self.thread_count {
+            println!("Finishied processing words of sice: {}", self.current_word_size);
+            self.current_word_size += 1;
+            self.word_size_counter = 0;
+        }
+    }
+
+    fn handle_processed_word_count(&mut self) {
+        // self.processed_word_count += count
+    }
+
+    fn handle_password_match(&mut self, password_match: PasswordMatch) {
+        println!("{}", password_match);
+
+    }
+
 }
 
 impl Default for Manager {
     fn default() -> Self {
         let (tx, rx) = mpsc::channel();
         Self {
-            current_word_size: Default::default(),
+            thread_count: Default::default(),
+            current_word_size: 1,
             word_size_counter: Default::default(),
             processed_word_count: Default::default(),
             processed_dictionary_count: Default::default(),
@@ -83,8 +118,8 @@ impl Default for Manager {
 pub fn run(settings: Settings) -> Result<(), CoreError> {
     // TODO: We need some sort of Instant here.
     let thread_settings = setup::ThreadSettings::from(&settings);
-    let mut manager = Manager::new();
+    let mut manager = Manager::new(settings.thread_count());
     manager.initialize(thread_settings);
-    manager.listen(settings.quiet());
+    manager.listen();
     Ok(())
 }
