22ce27e1300d3089a2c09cd117096a3cb89e5362
Johan Ekman <ekman-johan020@hotmail.com>
2023-03-25 16:24:40 +0100

    Moved all error related files

---
+++ src/error/mod.rs
@@ -0,0 +1,43 @@
+pub mod argument;
+pub mod core;
+
+use self::argument::ArgumentError;
+use self::core::CoreError;
+
+#[derive(Debug)]
+pub(super) enum Error {
+    CliError(ArgumentError),
+    CoreError(CoreError),
+}
+
+impl Error {
+    pub fn exit(&self) {
+        let exit_code = match self {
+            Error::CliError(e) => e.get_exit_code(),
+            Error::CoreError(e) => e.get_exit_code(),
+        };
+        println!("{}", self);
+        std::process::exit(exit_code);
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Error::CliError(e) => write!(f, "{}", e),
+            Error::CoreError(e) => write!(f, "{}", e),
+        }
+    }
+}
+
+impl From<ArgumentError> for Error {
+    fn from(error: ArgumentError) -> Self {
+        Error::CliError(error)
+    }
+}
+
+impl From<CoreError> for Error {
+    fn from(error: CoreError) -> Self {
+        Error::CoreError(error)
+    }
+}
