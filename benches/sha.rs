4dd58a65d43a7a35a321f018e0ca9ff3f95ddc10
Johan Ekman <ekman-johan020@hotmail.com>
2023-04-01 22:06:20 +0200

    Added benchmarks for SHA1 & SHA2

---
+++ benches/sha.rs
@@ -0,0 +1,91 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use hash::{sha1, sha2};
+
+fn sha1_benchmark(c: &mut Criterion) {
+    c.bench_function("sha1", |b| {
+        b.iter(|| {
+            let mut sha = sha1::Sha160::new();
+            sha.load(black_box(b"Benchmark"));
+            sha.run();
+            sha.extract();
+        })
+    });
+}
+
+fn sha2_224_benchmark(c: &mut Criterion) {
+    c.bench_function("sha2_224", |b| {
+        b.iter(|| {
+            let mut sha = sha2::Sha224::new();
+            sha.load(black_box(b"Benchmark"));
+            sha.run();
+            sha.extract();
+        })
+    });
+}
+
+fn sha2_256_benchmark(c: &mut Criterion) {
+    c.bench_function("sha2_256", |b| {
+        b.iter(|| {
+            let mut sha = sha2::Sha256::new();
+            sha.load(black_box(b"Benchmark"));
+            sha.run();
+            sha.extract();
+        })
+    });
+}
+
+fn sha2_384_benchmark(c: &mut Criterion) {
+    c.bench_function("sha2_384", |b| {
+        b.iter(|| {
+            let mut sha = sha2::Sha384::new();
+            sha.load(black_box(b"Benchmark"));
+            sha.run();
+            sha.extract();
+        })
+    });
+}
+
+fn sha2_512_benchmark(c: &mut Criterion) {
+    c.bench_function("sha2_512", |b| {
+        b.iter(|| {
+            let mut sha = sha2::Sha512::new();
+            sha.load(black_box(b"Benchmark"));
+            sha.run();
+            sha.extract();
+        })
+    });
+}
+
+fn sha2_512_224_benchmark(c: &mut Criterion) {
+    c.bench_function("sha2_512_224", |b| {
+        b.iter(|| {
+            let mut sha = sha2::Sha512_224::new();
+            sha.load(black_box(b"Benchmark"));
+            sha.run();
+            sha.extract();
+        })
+    });
+}
+
+fn sha2_512_256_benchmark(c: &mut Criterion) {
+    c.bench_function("sha2_512_256", |b| {
+        b.iter(|| {
+            let mut sha = sha2::Sha512_256::new();
+            sha.load(black_box(b"Benchmark"));
+            sha.run();
+            sha.extract();
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    sha1_benchmark,
+    sha2_224_benchmark,
+    sha2_256_benchmark,
+    sha2_384_benchmark,
+    sha2_512_benchmark,
+    sha2_512_224_benchmark,
+    sha2_512_256_benchmark
+);
+criterion_main!(benches);
